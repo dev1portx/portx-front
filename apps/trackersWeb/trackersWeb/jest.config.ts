2449e22c79e3bd9538d3508e8b9775f445771267
Axel <axel@bego.ai>
2025-04-02 15:37:29 -0600

    start project

---
+++ apps/trackersWeb/trackersWeb/jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  displayName: 'trackersWeb',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory:
+    '../../../coverage/apps/trackersWeb/trackersWeb',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
